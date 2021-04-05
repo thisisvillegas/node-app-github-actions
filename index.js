8932f9f6dad28ae2835d871cc544abfa91e21d6b
Andres Villegas <thisisvillegas@gmail.com>
2021-04-05 17:15:09 -0400

    added character

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Respond with "Hello World via Github Actions" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello Lauren via Github Actions");
+ return res.send("Hello Lauren via Github Actions!");
 });
 
 // listen to port 7000 by default
