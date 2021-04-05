93a33014c2cd2583066542b8d6df3b340e06c241
Andres Villegas <thisisvillegas@gmail.com>
2021-04-05 17:10:34 -0400

    removed character

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const app = require("../index");
 
 describe("GET /", () => {
   it("respond with Hello World via Github Actions", (done) => {
-    request(app).get("/").expect("Hello Lauren via Github Actions!", done);
+    request(app).get("/").expect("Hello Lauren via Github Actions", done);
   })
 });
