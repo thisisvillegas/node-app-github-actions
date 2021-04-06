e5a49a1e0aab1971bc133815295f0f919e401b8f
Andres Villegas <thisisvillegas@gmail.com>
2021-04-06 14:40:54 -0400

    updates secrets

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const app = require("../index");
 
 describe("GET /", () => {
   it("respond with Hello World via Github Actions", (done) => {
-    request(app).get("/").expect("Hola Lauren via Github Actions", done);
+    request(app).get("/").expect("Hola Lauren via Github Actions!", done);
   })
 });
